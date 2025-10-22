cf7742e4a1d801fcd53f0ff2974f4be39e9b8ba5
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-10-22 17:37:22 +0100

    went back

---
+++ Runtime/Networking/Network/Components/NetworkGUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace KadenZombie8.BIMOS.Networking
+{
+    public class NetworkGUI : MonoBehaviour
+    {
+        private string address = string.Empty;
+        private ushort port = 7777;
+        private ushort maxclients = 32;
+        private void OnGUI() {
+            GUILayout.Box("Network Status");
+            GUILayout.Label($"Server Running: {Network.Server.IsRunning}");
+            GUILayout.Label($"Client Connected: {Network.Client.IsConnected}");
+            GUILayout.Box("Network Manager");
+            GUILayout.Label("port");
+            port = ushort.Parse(GUILayout.TextField($"{port}"));
+            GUILayout.Label("max clients");
+            maxclients = ushort.Parse(GUILayout.TextField($"{maxclients}"));
+            if (GUILayout.Button("Start Server"))
+                Network.InitializeServer(maxclients, port);
+            if (GUILayout.Button("Start Host"))
+                Network.InitializeHost(maxclients, port);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Address");
+            address = GUILayout.TextField(address);
+            if (GUILayout.Button("Connect"))
+                Network.Connect(address, port);
+            GUILayout.EndHorizontal();
+        }
+    }
+}
