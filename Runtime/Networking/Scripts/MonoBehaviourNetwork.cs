0f5e180f7b05ea20471461e28d5cc86677c7fec8
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-10-30 11:44:45 +0000

    allocate dictionaries

---
+++ Runtime/Networking/Scripts/MonoBehaviourNetwork.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace KadenZombie8.BIMOS.Networking
+{
+    public class MonoBehaviourNetwork : MonoBehaviour
+    {
+        public NetworkView View {
+            get; internal set;
+        }
+    }
+}
--- Runtime/Networking/Scripts/NetworkGrip.cs
+++ Runtime/Networking/Scripts/NetworkGrip.cs
@@ -1,37 +1,20 @@
 using UnityEngine;
 using KadenZombie8.BIMOS.Rig;
-using System;
-using FishNet.Object;
-using FishNet;
-using FishNet.Connection;
-using FishNet.Broadcast;
 using System.Collections.Generic;
-using FishNet.Transporting;
 
 namespace KadenZombie8.BIMOS.Networking {
     [RequireComponent(typeof(Grabbable))]
-    public class NetworkGrip : MonoBehaviour {
+    public class NetworkGrip : MonoBehaviourNetwork {
         private static Dictionary<int, NetworkGrip> gripInstances = new Dictionary<int, NetworkGrip>();
-        public int NetId;
-        private NetworkObject networkObject;
+        public int id;
         private Grabbable grabbable;
         private Rigidbody rb;
 
         private void Awake() {
-            NetId = GetNextId();
-            gripInstances.Add(NetId, this);
+            id = gripInstances.AllocateId(this);
             rb = GetComponent<Rigidbody>();
             grabbable = GetComponent<Grabbable>();
-            networkObject = GetComponentInParent<NetworkObject>();
             grabbable.OnGrab += OnGrabbed;
-        }
-
-        private static int GetNextId() {
-            int id = 0;
-            while (gripInstances.ContainsKey(id)) {
-                id++;
-            }
-            return id;
         }
 
         private void Start() {
@@ -45,7 +28,7 @@ namespace KadenZombie8.BIMOS.Networking {
 
         [Client]
         private void SendGripTakeover() {
-            InstanceFinder.ClientManager.Broadcast(new GripTakeover(NetId));
+            InstanceFinder.ClientManager.Broadcast(new GripTakeover(id));
         }
 
         [Server]
